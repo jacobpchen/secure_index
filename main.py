02f155efab31303dd2963a1c24c9a47380778514
Jacob Chen <jacobpchen@gmail.com>
2020-12-06 11:59:28 -0500

    added build index function call in main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ keys = sse.keygen(20)
 print("These are the keys: " + str(keys))
 trapdoors = sse.trapdoor(keys, 'test')
 print("These are the trapdoors: " + str(trapdoors))
+build_index = sse.build_index('document1', keys, ['apples', 'bananas', 'oranges', 'pears', 'groceries', 'shopping', 'list'])
 
 # SearchableEncryptionScheme.keygen()
 
