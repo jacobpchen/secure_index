84d14b5bde77b6e60f9e1ce603bea3b3c00b36b4
Jacob Chen <jacobpchen@gmail.com>
2020-12-11 16:27:30 -0500

    finished implementing build index

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ print("These are the keys: " + str(keys))
 trapdoors = sse.trapdoor(keys, 'test')
 print("These are the trapdoors: " + str(trapdoors))
 build_index = sse.build_index('document1', keys, ['apples', 'bananas', 'oranges', 'pears', 'groceries', 'shopping', 'list'])
-
+print(type(build_index))
