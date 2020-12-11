6477cc96a55a6eeaaa4be09596ae53115cc9e0c3
Jacob Chen <jacobpchen@gmail.com>
2020-12-10 22:33:20 -0500

    cleaned up white space

--- main.py
+++ main.py
@@ -7,5 +7,3 @@ trapdoors = sse.trapdoor(keys, 'test')
 print("These are the trapdoors: " + str(trapdoors))
 build_index = sse.build_index('document1', keys, ['apples', 'bananas', 'oranges', 'pears', 'groceries', 'shopping', 'list'])
 
-# SearchableEncryptionScheme.keygen()
-
