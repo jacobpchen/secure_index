f36900afed08ee2e242ce2cfef761e83b23b327e
Jacob Chen <jacobpchen@gmail.com>
2020-12-20 16:55:17 -0500

    changed build_index to become a list of tuples

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from secure_index import SearchableEncryptionScheme
 sse = SearchableEncryptionScheme()
 keys = sse.keygen(20)
 print("These are the keys: " + str(keys))
-trapdoors = sse.trapdoor(keys, 'test')
-print("These are the trapdoors: " + str(trapdoors))
-build_index = sse.build_index('document1', keys, ['apples', 'bananas', 'oranges', 'pears', 'groceries', 'shopping', 'list'])
+# trapdoors = sse.trapdoor(keys, 'test')
+# print("These are the trapdoors: " + str(trapdoors))
+
+# Build index is a list of tuples containing the bloom filter and the document unique identifier.
+build_index = []
+
+build_index.append(sse.build_index('document1', keys, ['apples', 'bananas', 'oranges', 'pears', 'groceries', 'shopping', 'list']))
 print(type(build_index))
+print(build_index)
+
+trapdoors = sse.trapdoor(keys, 'bananas')
+
