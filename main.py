32095accac26c169a9b87e021917a956898b505e
Jacob Chen <jacobpchen@gmail.com>
2020-12-23 09:12:50 -0500

    added a function to read all files

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from secure_index import SearchableEncryptionScheme
 sse = SearchableEncryptionScheme()
 keys = sse.keygen(20)
 print("These are the keys: " + str(keys))
-# trapdoors = sse.trapdoor(keys, 'test')
-# print("These are the trapdoors: " + str(trapdoors))
+
+# Call function to open a directory and scan all doc files for unique words.
+# Calculate the average for noise
+sse.read_files()
+
 
 # Build index is a list of tuples containing the bloom filter and the document unique identifier.
 build_index = []
-
-build_index.append(sse.build_index('document1', keys, ['apples', 'bananas', 'oranges', 'pears', 'groceries', 'shopping', 'list']))
+build_index.append(sse.build_index('document1', keys, ['oranges', 'bananas', 'apples', 'pears', 'groceries', 'shopping', 'list']))
 print("This is the type for build index", type(build_index))
 print(build_index)
 
-trapdoors = sse.trapdoor(keys, 'oranges')
+
+
+# Stuck here - Codewords aren't matching up
+trapdoors = sse.trapdoor(keys, 'apples')
 print(sse.searchIndex(trapdoors, build_index))
